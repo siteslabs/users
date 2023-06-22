125901970bd28f0412a740a6d081f74e54b942f2
beksultan <beksultan.mamashov@gmail.com>
2023-06-22 15:20:45 +0600

    feat: tailwind extend: colors, borderRadius

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        default: {
+          5: "#F9FAFB",
+          10: "#EFEFF6",
+          20: "#EBEBF0",
+          40: "#C1C1CB",
+          50: "#9494A0",
+        },
+        "blue-grey-regular": "#424F5E",
+        "danger-regular": "#FF9E90",
+        "button-bg": "#32C076",
+        "button-bg-hover": "#28ce78",
+      },
+      borderRadius: {
+        default: "0.625rem",
+      },
+    },
   },
   plugins: [],
 };

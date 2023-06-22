cd3892022ea639525cd056d9d5f416f9abdc9f95
beksultan <beksultan.mamashov@gmail.com>
2023-06-22 18:10:26 +0600

    feat: Sidebar Icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,31 @@
-import { ReactComponent as UserIcon } from "./user.svg";
+import { ReactComponent as AvatarIcon } from "./avatar.svg";
 import { ReactComponent as DotsIcon } from "./dots.svg";
 import { ReactComponent as SearchIcon } from "./search.svg";
 import { ReactComponent as BarsIcon } from "./bars.svg";
+import { ReactComponent as ChatIcon } from "./chat.svg";
+import { ReactComponent as CheckedListIcon } from "./checked-list.svg";
+import { ReactComponent as CoinIcon } from "./coin.svg";
+import { ReactComponent as ImageIcon } from "./image.svg";
+import { ReactComponent as ListIcon } from "./list.svg";
+import { ReactComponent as LogoutIcon } from "./logout.svg";
+import { ReactComponent as UsersIcon } from "./users.svg";
+import { ReactComponent as UserIcon } from "./user.svg";
+import { ReactComponent as PieIcon } from "./pie.svg";
+import { ReactComponent as LogoIcon } from "./logo.svg";
 
-export { UserIcon, DotsIcon, SearchIcon, BarsIcon };
+export {
+  AvatarIcon,
+  DotsIcon,
+  SearchIcon,
+  BarsIcon,
+  ChatIcon,
+  CheckedListIcon,
+  CoinIcon,
+  ImageIcon,
+  ListIcon,
+  LogoutIcon,
+  UsersIcon,
+  UserIcon,
+  PieIcon,
+  LogoIcon,
+};

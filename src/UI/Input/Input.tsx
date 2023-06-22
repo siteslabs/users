eca3dc0f0296c2862db9a5a78e14d58dcbe0ac4c
beksultan <beksultan.mamashov@gmail.com>
2023-06-22 19:05:20 +0600

    fix: Input spread props

--- src/UI/Input/Input.tsx
+++ src/UI/Input/Input.tsx
@@ -4,8 +4,7 @@ interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
   rightIcon?: React.ReactElement;
 }
 
-const Input: React.FC<IInputProps> = (props) => {
-  const { rightIcon } = props;
+const Input: React.FC<IInputProps> = ({ rightIcon, ...props }) => {
   return (
     <div className="flex w-full items-center justify-between rounded-default border border-default-40 bg-default-5 px-[0.813rem] py-2">
       <input className="w-full border-none outline-none" {...props} />
